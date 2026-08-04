03d4c7adbac0b197b54ad739a512bb00c5a56357
velvetleaf420L <velvetleaf420L@gmail.com>
2026-08-04 12:41:32 +1000

    update product system

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
+// ============================
 // Mobile Menu
+// ============================
+
 
 function openMenu(){
 
@@ -11,6 +14,7 @@ function openMenu(){
     }
 
 }
+
 
 
 
@@ -28,22 +32,269 @@ function closeMenu(){
 
 
 
-// close when clicking outside
+
+
+// Close menu when clicking outside
+
 
 document.addEventListener("click", function(e){
+
 
     const menu = document.getElementById("mobileMenu");
 
     const button = document.querySelector(".menu-btn");
 
 
-    if(menu && 
-       !menu.contains(e.target) && 
-       !button.contains(e.target)){
 
-        menu.classList.remove("active");
+    if(menu && button){
+
+
+        if(
+
+            !menu.contains(e.target) &&
+
+            !button.contains(e.target)
+
+        ){
+
+            menu.classList.remove("active");
+
+        }
+
 
     }
 
 
 });
+
+
+
+
+
+
+
+
+// ============================
+// Product Details System
+// ============================
+
+
+const products = {
+
+
+
+    ak:{
+
+        name:"AK",
+
+        image:"images/products/ak.png",
+
+        description:"Candy Bear Collection item.",
+
+        info:"Product information will appear here."
+
+    },
+
+
+
+
+    mango:{
+
+        name:"Mango",
+
+        image:"images/products/mango.png",
+
+        description:"Candy Bear Collection item.",
+
+        info:"Product information will appear here."
+
+    },
+
+
+
+
+    "9lb-hammer":{
+
+        name:"9LB Hammer",
+
+        image:"images/products/9lb-hammer.png",
+
+        description:"Candy Bear Collection item.",
+
+        info:"Product information will appear here."
+
+    },
+
+
+
+
+
+    superboof:{
+
+        name:"Super Boof",
+
+        image:"images/products/superboof.png",
+
+        description:"Candy Bear Collection item.",
+
+        info:"Product information will appear here."
+
+    },
+
+
+
+
+
+    lemon:{
+
+        name:"Lemon Kush",
+
+        image:"images/products/lemon.png",
+
+        description:"Candy Bear Collection item.",
+
+        info:"Product information will appear here."
+
+    },
+
+
+
+
+
+    "white-rhino":{
+
+        name:"White Rhino",
+
+        image:"images/products/white-rhino.png",
+
+        description:"Candy Bear Collection item.",
+
+        info:"Product information will appear here."
+
+    },
+
+
+
+
+
+    gummies:{
+
+        name:"Gummies",
+
+        image:"images/products/gummies.png",
+
+        description:"Candy Bear Collection item.",
+
+        info:"Product information will appear here."
+
+    },
+
+
+
+
+
+    cookies:{
+
+        name:"Cookies",
+
+        image:"images/products/cookies.png",
+
+        description:"Candy Bear Collection item.",
+
+        info:"Product information will appear here."
+
+    },
+
+
+
+
+
+    "thc-carts":{
+
+        name:"THC Carts",
+
+        image:"images/products/thc-carts.png",
+
+        description:"Candy Bear Collection item.",
+
+        info:"Product information will appear here."
+
+    }
+
+
+};
+
+
+
+
+
+
+
+// Read product ID from URL
+
+
+const params = new URLSearchParams(window.location.search);
+
+
+const productID = params.get("id");
+
+
+
+
+
+
+// Update product page
+
+
+if(productID && products[productID]){
+
+
+    const image =
+    document.getElementById("productImage");
+
+
+    const name =
+    document.getElementById("productName");
+
+
+    const description =
+    document.getElementById("productDescription");
+
+
+    const info =
+    document.getElementById("productInfo");
+
+
+
+    if(image){
+
+        image.src = products[productID].image;
+
+    }
+
+
+
+    if(name){
+
+        name.innerHTML = products[productID].name;
+
+    }
+
+
+
+    if(description){
+
+        description.innerHTML = products[productID].description;
+
+    }
+
+
+
+    if(info){
+
+        info.innerHTML = products[productID].info;
+
+    }
+
+
+}
